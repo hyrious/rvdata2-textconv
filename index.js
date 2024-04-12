efa422ee6bb6c93248370f864841c1a617802d3b
hyrious <hyrious@outlook.com>
2024-04-12 11:48:32 +0800

    update dependencies

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import fs from 'node:fs'
+import path from 'node:path'
 import zlib from 'node:zlib'
+import crypto from 'node:crypto'
 import {load} from '@hyrious/marshal'
 
 /**
  * Reads Scripts.rvdata2 and returns `$RGSS_SCRIPTS`.
+ * If the file is not Scripts.rvdata2, it returns an array of `[{ code: "hash" }]`.
  * @param  {string} full_path path to Scripts.rvdata2, will be used in fs.readFile()
  * @return {Array<[magic: number, title: string, raw: ArrayBuffer, code: string]>}
  */
 export function load_scripts_rvdata2(full_path) {
 	const decoder = new TextDecoder()
-	const buffer = new Uint8Array(fs.readFileSync(full_path)).buffer
+	const buffer = fs.readFileSync(full_path)
 	const result = []
-	for (const [magic, title_, code_] of load(buffer, {decodeString: false})) {
+	if (!full_path.endsWith('Scripts.rvdata2')) {
+		const hash = crypto.createHash('sha1').update(buffer).digest('hex')
+		result.push([0, path.basename(full_path), Uint8Array.of().buffer, hash])
+		return result;
+	}
+	for (const [magic, title_, code_] of load(buffer, {string: 'binary'})) {
 		const title = decoder.decode(title_)
-		const code = zlib.inflateSync(code_).toString('utf8')
+		const code = zlib.inflateSync(code_).toString('utf8').replace(/\r?\n/g, '\n')
 		result.push([magic, title, code_, code])
 	}
 	return result
